05889baedc8d42427f6f7c51c957ee46de0f8268
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-08-16 15:38:54 -0700

    Add Ent class with instance variables

--- ent.rb
+++ ent.rb
@@ -1,2 +1,24 @@
+# ent_data = {
+# 	aliases: ['Onodrim'],
+# 	created_by: (Yavanna.new.name),
+# 	location: 'Fangorn Forest',
+# 	languages: ['Entish', 'Westron'],
+# 	height: 15 
+# }
+
+
+
 class Ent
+	attr_reader :ailiases
+	
+	def initialize
+		@ailiases = ['Onodrim']
+		#Make sure to add a name attribute to the Yavanna class so next
+		#line will work
+		@created_by = (Yavanna.new.name)
+		@location = 'Fangorn Forest'
+		@height = 15
+
+	end
+
 end
