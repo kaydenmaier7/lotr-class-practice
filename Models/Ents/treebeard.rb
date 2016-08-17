74b57c10a9393181089964209016f7fb338cd6bb
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-08-17 00:05:09 -0700

    Add ent_wife, height, and titles

--- Models/Ents/treebeard.rb
+++ Models/Ents/treebeard.rb
@@ -1,4 +1,12 @@
 # Dir["./Models/*.rb"].each { |file| require file } 
-class Treebeard < Ent 
+require_relative 'fimbrethil'
 
+class Treebeard < Ent 
+	attr_reader :ent_wife
+	def initialize
+		super
+		@titles += ['Eldest', 'Oldest of the Ents']
+		@ent_wife = Fimbrethil.new.name
+		@height = 15.4
+	end
 end
