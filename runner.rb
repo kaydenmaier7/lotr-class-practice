cc9ac22b7c2ce73469bb0e9ec83a54094f166e11
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-08-16 23:04:30 -0700

    Add require to all files in folder

---
+++ runner.rb
@@ -0,0 +1,45 @@
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/ent'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/treebeard'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/birchseed'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/fimbrethil'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/fladrif'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/leaflock'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/quickbeam'
+
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ainur/ainu'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ainur/Maiar/maia'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ainur/Maiar/Balrogs/balrog'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ainur/Maiar/Istari/istar'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ainur/Valar/vala'
+
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Deities/eru_illuvitar'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Deities/tom_bombadil'
+
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Dwarves/dwarf'
+
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Elves/elf'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Elves/lorien_elf'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Elves/mirkwood_elf'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Elves/rivendell_elf'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Elves/Orcs/orc'
+
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Humans/human'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Humans/gondor'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Humans/rohan'
+require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Humans/Hobbits/hobbit'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
