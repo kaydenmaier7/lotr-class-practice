1e131c1de6dff69847ac42178b1f4b90a0afc68b
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-08-16 18:54:14 -0700

    Add more instance variables to class

--- Models/Ents/ent.rb
+++ Models/Ents/ent.rb
@@ -12,13 +12,16 @@ class Ent
 	attr_reader :ailiases
 	
 	def initialize
+		@titles = []
 		@ailiases = ['Onodrim']
 		#Make sure to add a name attribute to the Yavanna class so next
 		#line will work
 		@created_by = (Yavanna.new.name)
 		@location = 'Fangorn Forest'
 		@height = 15
-
+		@languages = ['Entish', 'Westron']
+		@tree_type = 'Huorn'
+	
 	end
 
 end
