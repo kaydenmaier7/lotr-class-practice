98324d26292b92be0f44383b4fe240433de05cdf
Ryker <ryker.reed3@gmail.com>
2016-08-17 22:16:35 -0700

    Adds pics of tengwar letters and preliminary files for translation from english to tengwar.

---
+++ tengwar/tengwar.rb
@@ -0,0 +1,40 @@
+TENGWAR_IMAGES = []
+
+class Tengwar
+
+  def initialize(roman_consonant)
+    @roman_consonant = roman_consonant
+  end
+
+  def roman_to_tengwar
+    @tengwar = case @roman_consonant
+      when 'b' then #
+      when 'd' then #
+      when 'f' then #
+      when 'g' then #
+      when 'h' then #
+      when 'j' then #
+      when 'k' then #
+      when 'l' then #
+      when 'm' then #
+      when 'n' then #
+      when 'p' then #
+      when 'r' then #
+      when 's' then #
+      when 't' then #
+      when 'v' then #
+      when 'w' then #
+      when 'y' then #
+      when 'z' then #
+      when 'sh' then #
+      when 'ch' then #
+      when 'dj' then #
+      when 'ng' then #
+      when 'th' then #
+      when 'dh' then #
+      when 'ny' then #
+    end
+  end
+
+
+end
