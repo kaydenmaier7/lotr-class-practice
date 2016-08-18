c787ac3b606e1f68370e86cf87580263e85adec0
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-08-17 23:30:32 -0700

    Add alternate_forms which is an instance of TomBombadil class

--- models/Deities/eru_illuvitar.rb
+++ models/Deities/eru_illuvitar.rb
@@ -1,8 +1,10 @@
+require_relative 'tom_bombadil'
+
 class EruIlluvitar
-	attr_reader :lifespan
+	attr_reader :lifespan, :alternate_forms
 
 	def initialize
 		@lifespan = Float::INFINITY
-	
+		@alternate_forms = TomBombadil.new
 	end
 end
