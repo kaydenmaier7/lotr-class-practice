54543450165219c4f3587b571b89fba283cf2bdd
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-08-18 19:01:28 -0700

    Add maiar and ARATAR varialbes

--- Models/Ainur/Valar/vala.rb
+++ Models/Ainur/Valar/vala.rb
@@ -3,11 +3,14 @@ require_relative '../ainu'
 class Vala < Ainu
 	attr_reader :location, :weapon, :powers
 	
+	ARATAR = ['Manwë', 'Varda', 'Ulmo', 'Yavanna', 'Aulë', 'Mandos', 'Nienne', 'Oromë']
 	def initialize
 		super
 		@titles += %w[Powers\ of\ Arda Lords\ of\ the\ Valar Queens\ of\ the\ Valar]
 		@location = 'Valinor'
-		@weapon = %q[Powers of the Valar]
+		@weapon = 'Powers of the Valar'
 		@powers = @weapon
+		@maiar = []
+		# @aratar_index =  
 	end
 end
