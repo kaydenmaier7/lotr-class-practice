98c93c192d4d4235bc85515480307e927e4e29d0
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-08-18 19:58:32 -0700

    Refactor file

--- runner.rb
+++ runner.rb
@@ -1,31 +1,31 @@
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/treebeard'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/birchseed'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/fimbrethil'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/skinbark'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/leaflock'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ents/quickbeam'
+require './Models/Ents/treebeard'
+require './Models/Ents/birchseed'
+require './Models/Ents/fimbrethil'
+require './Models/Ents/skinbark'
+require './Models/Ents/leaflock'
+require './Models/Ents/quickbeam'
 
-# require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ainur/ainu'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ainur/Maiar/maia'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ainur/Maiar/Balrogs/balrog'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ainur/Maiar/Istari/istar'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Ainur/Valar/vala'
+require './Models/Ainur/Maiar/maia'
+require './Models/Ainur/Maiar/Balrogs/balrog'
+require './Models/Ainur/Maiar/Istari/istar'
 
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Deities/eru_illuvitar'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Deities/tom_bombadil'
+require './Models/Ainur/Valar/vala'
 
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Dwarves/dwarf'
+require './Models/Deities/eru_illuvitar'
+require './Models/Deities/tom_bombadil'
 
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Elves/elf'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Elves/lorien_elf'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Elves/mirkwood_elf'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Elves/rivendell_elf'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Elves/Orcs/orc'
+require './Models/Dwarves/dwarf'
 
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Humans/human'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Humans/gondor'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Humans/rohan'
-require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Humans/Hobbits/hobbit'
+require './Models/Elves/elf'
+require './Models/Elves/lorien_elf'
+require './Models/Elves/mirkwood_elf'
+require './Models/Elves/rivendell_elf'
+require './Models/Elves/Orcs/orc'
+
+require './Models/Humans/human'
+require './Models/Humans/gondor'
+require './Models/Humans/rohan'
+require './Models/Humans/Hobbits/hobbit'
 
 # p a = Fimbrethil.new
 # puts a.ent_husband
@@ -34,14 +34,14 @@ require '/Users/Noah/Desktop/DBC/lotr-class-practice/Models/Humans/Hobbits/hobbi
 # p birch.tree_type
 
 # leaflock = Leaflock.new
-# p leaflock.ailiases
+# p leaflock.aliases
 
 # skinbark = Skinbark.new
 # p skinbark.titles
 # p skinbark.tree_type
 
 # quickbeam = Quickbeam.new
-# p quickbeam.ailiases
+# p quickbeam.aliases
 # p quickbeam.tree_type
 
 # a = Ainu.new
