7e13e61d2c71800893f0265319b3f8f4b9c5f598
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-08-19 20:36:01 -0700

    Add wireframe for aulë

--- Models/Ainur/Valar/aulë.rb
+++ Models/Ainur/Valar/aulë.rb
@@ -3,7 +3,13 @@ require_relative 'vala'
 class Aulë < Vala
 	
 	def initialize
-		
+		super
+		@titles += %w[The\ Smith]
+		@aliases += %w[Mahal Návatar Oli]
+		@powers = 'Smithing'
+		@spouse = Yavanna
+		# @maiar = [Sauron, Saruman]
+		@created += [Dwarf]
 	end
 	
 	
