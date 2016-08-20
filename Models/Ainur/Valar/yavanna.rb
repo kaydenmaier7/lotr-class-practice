eb6391ad358b7448fc07a891f4b37092f8edad47
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-08-19 19:42:10 -0700

    Add class wireframe

--- Models/Ainur/Valar/yavanna.rb
+++ Models/Ainur/Valar/yavanna.rb
@@ -1,8 +1,17 @@
 require_relative 'vala'
 
 class Yavanna < Vala
+	attr_reader :created
 	
 	def initialize
+		super
+		@titles += %w[Queen\ of\ the\ Earth Giver\ of\ Fruits]
+		@aliases += %w[Kementári Palúrien Ivon]
+		@powers = 'Earth'
+		# @maiar = [Radagast]
+		@spouse = Aulë
+		@sister = Vána
+		@created = Ent 
 		
 	end
 	
