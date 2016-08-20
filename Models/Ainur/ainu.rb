9bc8a59186e3a68f7acb911de6c09938bc291d6c
kaydenwilliams7 <kaydenwilliams7@gmail.com>
2016-08-19 18:55:46 -0700

    Add powers variable

--- Models/Ainur/ainu.rb
+++ Models/Ainur/ainu.rb
@@ -1,11 +1,12 @@
 
 class Ainu 
-	attr_reader :name, :lifespan, :titles, :created_by
+	attr_reader :name, :lifespan, :titles, :powers, :created_by
 
 	def initialize
 		@lifespan = Float::INFINITY
 		@name = self.class.to_s
 		@titles = ['Holy Ones']
+		@powers = 'Powers of the Ainur'
 		@created_by = EruIlluvitar
 	end
 end
